cce3863b19246a037a6a5faed8270a275fb8a534
RK13 <eduard_barbu_13@yahoo.com>
2025-01-22 15:59:11 +0200

    Add Spring Boot Project

---
+++ homework/src/main/java/com/ing/hubs/project/dto/response/GradeDto.java
@@ -0,0 +1,17 @@
+package com.ing.hubs.project.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GradeDto {
+    private String studentUsername;
+    private String courseName;
+    private String teacherUsername;
+    private Integer grade;
+}
